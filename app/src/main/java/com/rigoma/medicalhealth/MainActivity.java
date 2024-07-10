7e13efeae60f7de08c2be137e21f6f5d264609ca
Optimus <74502300+lordm121@users.noreply.github.com>
2024-07-11 01:10:55 +0300

    Update MainActivity.java

--- app/src/main/java/com/rigoma/medicalhealth/MainActivity.java
+++ app/src/main/java/com/rigoma/medicalhealth/MainActivity.java
@@ -1,12 +1,10 @@
 package com.rigoma.medicalhealth;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.ValueCallback;
@@ -16,6 +14,7 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
@@ -30,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         View splashScreen = findViewById(R.id.splash_screen);
         View loadingScreen = findViewById(R.id.loading_screen);
         webView = findViewById(R.id.web_view);
+        Button refreshButton = findViewById(R.id.refresh_button);
 
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -37,35 +37,30 @@ public class MainActivity extends AppCompatActivity {
         webView.setHorizontalScrollBarEnabled(false);
         webView.loadUrl(webUrl);
 
-        //open other links that are pressed in the same webview instead of sending it to another app/activity.
+        // Open other links in the same WebView
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
                 view.loadUrl(request.getUrl().toString());
-
                 return true;
             }
 
-            //when a new link starts loading, this method will run.
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
                 super.onPageStarted(view, url, favicon);
                 loadingScreen.setVisibility(View.VISIBLE);
             }
 
-            //when the new link that started loading finishes and is fully ready, this method will run.
             @Override
             public void onPageFinished(WebView view, String url) {
                 splashScreen.setVisibility(View.GONE);
                 loadingScreen.setVisibility(View.GONE);
             }
 
-            //when an error occurs while loading a link, this method will run.
             @Override
             public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
                 super.onReceivedError(view, request, error);
-
-                //insert failed to load screen.
+                // Insert failed to load screen logic here
             }
         });
 
@@ -77,24 +72,29 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        // Refresh button logic
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webView.reload();
+            }
+        });
     }
 
     private void openImageChooser() {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.setType("image/*");
-
         startActivityForResult(Intent.createChooser(intent, "Choose File"), 1);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         if (requestCode == 1) {
             if (mUploadMessage == null)
                 return;
-
             if (resultCode == RESULT_OK) {
                 if (data != null) {
                     Uri[] result = new Uri[]{data.getData()};
@@ -105,19 +105,16 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 mUploadMessage.onReceiveValue(null);
             }
-
             mUploadMessage = null;
         }
     }
 
-    //go back to the previous state in the webview if it exists. if not, then leave the activity.
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
             webView.goBack();
             return true;
         }
-
         return super.onKeyDown(keyCode, event);
     }
 }
